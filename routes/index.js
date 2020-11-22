8b5bfecbe1f33b6f0ed1e67310b787bf1ffcaa60
Ahzed11 <a.zenon@student.uclouvain.be>
2020-11-22 19:02:23 +0100

    Recherche d'articles

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,16 @@ const Article = require('../models/article');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const articles = await Article.find().populate('author').limit(5).sort('-createdAt');
-  res.render('index', {userName: req.session.userName, articles: articles});
+  let articles;
+  const searchTerm = req.query.searchTerm;
+
+  if(searchTerm){
+    articles = await Article.find({$text: {$search: searchTerm}}).populate('author').sort('-createdAt');
+  } else {
+    articles = await Article.find().populate('author').sort('-createdAt');
+  }
+
+  res.render('index', {userName: req.session.userName, articles: articles, searchTerm: searchTerm});
 });
 
 module.exports = router;
