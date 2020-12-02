c5699eb00f47f7393fd062188425ecb4efb005b1
Alexandre Zenon <a.zenon@student.uclouvain.be>
2020-12-02 13:41:59 +0100

    Implémentation de la section tags populaires

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const Article = require('../models/article');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
+  // Articles
   let articles;
   const searchTerm = req.query.searchTerm;
 
@@ -13,8 +14,10 @@ router.get('/', async function(req, res, next) {
     articles = await Article.find().populate('author').sort('-createdAt').limit(8);
   }
 
-  let popularArticles = await Article.aggregate([
-    { $project: {
+  // Popular articles
+  const popularArticles = await Article.aggregate([
+    {
+      $project: {
         "title": 1,
         "shortDescription": 1,
         "tags": 1,
@@ -32,12 +35,33 @@ router.get('/', async function(req, res, next) {
     { $sort: { "popularity": -1 } },
     { $limit: 2 }
   ]);
-  console.log(popularArticles);
+
+  // Popular tags
+  const tags = await Article.aggregate([
+    {
+      $project: {
+        "tagsWithComma": { $concat: [ "$tags", ","] },
+      }
+    },
+  ]);
+
+  let tagsConcat = ''
+  tags.forEach((e) => {
+    tagsConcat += e.tagsWithComma;
+  })
+  let tagArray = tagsConcat.replace(/ /g, '').split(',');
+  let frequency = {};
+  tagArray.forEach(tag => frequency[tag] ? frequency[tag]++ : frequency[tag] = 1);
+  const keyVals = Object.entries(frequency);
+  let sortedNestedArray = keyVals.sort((a, b) => a[1] - b[1]);
+  let flattenedArray = sortedNestedArray.map((arr) => arr[0]);
+  let popularTags = flattenedArray.reverse().slice(0, 8);
 
   res.render('index', {
     userName: req.session.userName,
     articles: articles,
     popularArticles: popularArticles,
+    popularTags: popularTags,
     searchTerm: searchTerm
   });
 });
