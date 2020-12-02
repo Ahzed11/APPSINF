fe65038309175015f3883642fb98bd307523e438
Alexandre Zenon <a.zenon@student.uclouvain.be>
2020-12-02 12:24:38 +0100

    Implémentation de la section articles populaires

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,38 @@ router.get('/', async function(req, res, next) {
   const searchTerm = req.query.searchTerm;
 
   if(searchTerm){
-    articles = await Article.find({$text: {$search: searchTerm}}).populate('author').sort('-createdAt');
+    articles = await Article.find({$text: {$search: searchTerm}}).populate('author').sort('-createdAt').limit(8);
   } else {
-    articles = await Article.find().populate('author').sort('-createdAt');
+    articles = await Article.find().populate('author').sort('-createdAt').limit(8);
   }
 
-  res.render('index', {userName: req.session.userName, articles: articles, searchTerm: searchTerm});
+  let popularArticles = await Article.aggregate([
+    { $project: {
+        "title": 1,
+        "shortDescription": 1,
+        "tags": 1,
+        "author": 1,
+        "createdAt": 1,
+        "slug": 1,
+        "popularity": {
+          $add : [
+            { $size: "$comments" },
+            { $size: "$reactions" }
+          ]
+        }
+      }
+    },
+    { $sort: { "popularity": -1 } },
+    { $limit: 2 }
+  ]);
+  console.log(popularArticles);
+
+  res.render('index', {
+    userName: req.session.userName,
+    articles: articles,
+    popularArticles: popularArticles,
+    searchTerm: searchTerm
+  });
 });
 
 module.exports = router;
